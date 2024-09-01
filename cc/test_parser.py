5622f6dd4a256992fc9fcf11bd70b647cb41f28d
mm <mm@viltstigen.se>
2024-09-01 14:19:20 +0200

    Updates

--- cc/test_parser.py
+++ cc/test_parser.py
@@ -1,17 +1,16 @@
 from sly import Parser
 import lexer
 import logging
+import sys
+
 
 class CCParser(Parser):
     start = 'translation_unit'
     tokens = lexer.CLexer.tokens
     debugfile = 'test_parser.out'
 
-    def plog(self):
-        def function_wrapper():
-            log.debug(f'{self.production}\n  {self.symstack}')
-
-        return function_wrapper
+    def dump(self):
+        log.debug(f'{self.production}\n  {self.symstack}')
 
     def error(self, p):
         print(f'\n*** ERROR ***\n')
@@ -25,61 +24,50 @@ class CCParser(Parser):
         else:
             print("Syntax error at EOF")
 
-    @plog
+
     @_('declaration')
-    def translation_unit(self, p): pass
+    def translation_unit(self, p): self.dump()
 
-    @plog
     @_('type_specifier')
-    def declaration(self, p): pass
+    def declaration(self, p): self.dump()
 
-    @plog
     @_('VOID', 'CHAR', 'SHORT', 'INT', 'LONG', 'FLOAT', 'DOUBLE', 'SIGNED', 'UNSIGNED',
        'struct_or_union_specifier'
        )
-    def type_specifier(self, p): pass
+    def type_specifier(self, p): self.dump()
 
-    @plog
     @_('struct_or_union ID "{" struct_declaration_list "}"')
-    def struct_or_union_specifier(self, p): pass
+    def struct_or_union_specifier(self, p): self.dump()
 
-    @plog
     @_('STRUCT', 'UNION')
-    def struct_or_union(self, p): pass
+    def struct_or_union(self, p): self.dump()
 
-    @plog
     @_('struct_declaration',
        'struct_declaration_list struct_declaration'
        )
-    def struct_declaration_list(self, p): pass
+    def struct_declaration_list(self, p): self.dump()
 
-    @plog
     @_('struct_declarator',
        'struct_declarator_list "," struct_declarator')
-    def struct_declarator_list(self, p): pass
+    def struct_declarator_list(self, p): self.dump()
 
-    @plog
     @_('struct_declarator_list ";"')
-    def struct_declaration(self, p): pass
+    def struct_declaration(self, p): self.dump()
 
-    @plog
     @_('declarator')
-    def struct_declarator(self, p): pass
+    def struct_declarator(self, p): self.dump()
 
-    @plog
     @_('direct_declarator')
-    def declarator(self, p): pass
+    def declarator(self, p): self.dump()
 
-    @plog
     @_('ID',
        'direct_declarator "[" constant_expression "]"',
        'direct_declarator "[" "]"',
        )
-    def direct_declarator(self, p): pass
+    def direct_declarator(self, p): self.dump()
 
-    @plog
     @_('ID', 'CONSTANT')
-    def constant_expression(self, p): pass
+    def constant_expression(self, p): self.dump()
 
 
 if __name__ == '__main__':
@@ -88,6 +76,12 @@ if __name__ == '__main__':
 
     logging.basicConfig(filename='test_parser.log', filemode='w', level=logging.DEBUG, format='%(message)s')
     log = logging.getLogger('CC_SLYLogger')
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
 
     lex = lexer.CLexer()
     parser = CCParser()
