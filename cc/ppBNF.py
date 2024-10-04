20a7c3f9687fba29b8fbe046bcc663a50d4fe29c
mm <mats.melander@gmail.com>
2024-10-04 09:58:02 +0200

    Updates

--- cc/ppBNF.py
+++ cc/ppBNF.py
@@ -4,31 +4,25 @@
 # Mats Melander 2023-07-31
 __author__ = 'mm'
 
-from sly import Lexer
-
-
-class BNFLexer(Lexer):
-
-    tokens = {DEF, EXPR}
-
-    literals = {'@', '_', '(', ')', ','}
-
-    ignore = ' \t'
-
-
-    DEF = 'def'
-    EXPR = r"'(\w+)'"
-#    EXPR = r"([\"'])((?:\\\1|(?:(?!\1)).)*)(\1)"
-#    EXPR = /'[^'"]*'(?=(?:[^"]*"[^"]*")*[^"]*$)/g
-
-    def error(self, t):
-        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
-        self.index += 1
 
 if __name__ == '__main__':
     with open('test_parser.py') as f:
         data = f.read()
 
-    lexer = BNFLexer()
-    for tok in lexer.tokenize(data):
-        print(tok)
+    rules = []
+    bnf_flag = False
+    def_flag = False
+
+    for ch in data:
+        if ch == '@':
+            bnf_flag = True
+            rules.append(ch)
+            continue
+        if bnf_flag:
+            rules[-1] += ch
+            bnf_flag = ch != ')'
+            continue
+
+    for r in rules:
+        print(r)
+
