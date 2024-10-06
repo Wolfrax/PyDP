9478af7bb28e4300459ec818fbfbacf60c525fb9
mm <mm@viltstigen.se>
2024-10-06 10:54:11 +0200

    Updates

--- cc/ppBNF.py
+++ cc/ppBNF.py
@@ -7,22 +7,57 @@ __author__ = 'mm'
 
 if __name__ == '__main__':
     with open('test_parser.py') as f:
-        data = f.read()
+        text = f.read().rstrip()
 
     rules = []
-    bnf_flag = False
+    at_flag = False
     def_flag = False
 
-    for ch in data:
-        if ch == '@':
-            bnf_flag = True
-            rules.append(ch)
-            continue
-        if bnf_flag:
-            rules[-1] += ch
-            bnf_flag = ch != ')'
+    # https://stackoverflow.com/questions/31228467/is-in-the-python-something-like-method-findnext-in-string
+    pos = -1
+    while True:
+        pos = text.find('@_(', pos + 1)
+        if pos == -1:
+            break
+        lhs_ind = text.find(')', pos + 1)
+        lhs = (text[pos+3:lhs_ind].
+               replace(')', '').
+               replace('\n', '').
+               replace('  ', '').
+               replace('\'', ''))
+        print(lhs)
+
+
+    for ch in text:
+        if ch.isspace():
             continue
 
-    for r in rules:
-        print(r)
+        if ch == '@' and not at_flag:
+            at_flag = True
+            prod = {'rhs': '', 'lhs': ''}  # new production
+            rhs = ''  # right hand side of production
+            lhs = ''  # left hand side of production
+            continue  # skip @-char
+        if at_flag:
+            if ch == '(' or ch == '_':
+                continue  # skip ( and _
+            if ch == ')':
+                at_flag = False  # done with rhs of production
+                prod['rhs'] = rhs
+                def_flag = True
+                continue
+            else:
+                rhs += ch
+        if def_flag:
+            if ch == '(':
+                def_flag = False  # done with lhs of production
+                prod['lhs'] = lhs
+                rules.append(prod)
+                continue
+            if  ch == 'd' or ch == 'e' or ch == 'f':
+                continue  # skip def
+            lhs += ch
+
+#    for r in rules:
+#        print(r)
 
